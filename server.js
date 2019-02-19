2ded256368747f7402e285e78695779c83419c11
SeaCrap <2902933052@qq.com>
2019-02-19 23:17:32 +0800

    获取歌曲外链

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ var server = http.createServer(function(request, response){
     
     let {accessKey,secretKey} = config
     var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-    console.log(mac)
     var options = {
       scope: 'music-demo',
     };
